0f3af49037e9b74ae67d8a89f078c578c8a3dfac
Александр Горячев <@local.itstep>
2019-04-05 21:14:29 +0600

    Добавьте файлы проекта.

---
+++ strings/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Strings_.h"
+using namespace std;
+
+// передачa объекта в функцию по значению
+void f(string_ st) {
+	st.append("!");
+}
+
+string_ getN() {
+	string_ res("N");
+	return res;
+}
+
+void main()
+{
+	/*string_ s("hello");
+	s.print();
+	s.setString("hollywood");
+	s.print();
+	s.append(" undead");
+	s.print();
+	s.setString("My own hell");
+	cout << s.equal("My own hell");*/
+
+
+	
+
+	system("pause");
+}
---
+++ strings/Strings_.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class string_
+{
+public:
+	string_();
+	string_(const char*s);
+	void setString(const char *s);
+
+	const char* getString();
+	int getLength();
+	void append(const char*s);
+	void print();
+	void clear();
+	bool equal(const char *s);
+	~string_();
+	string_(const string_ &obj);
+
+private:
+	char *str;
+	int length;
+};
+
